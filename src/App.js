b7fa9f753f5bd75b9a476a882640b245e9aedd81
Umair Farooq <umair.farooq@osf-commerce.com>
2019-05-21 11:32:06 +0500

    React by Example: GuestList component added. prop types introduced

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import GuestList from './GuestList';
 
 class App extends Component {
 
@@ -7,11 +8,15 @@ class App extends Component {
         guests: [
             {
                 name: 'Tresure',
-                confirmed: false
+                isConfirmed: false
             },
             {
                 name: 'Nic',
-                confirmed: true
+                isConfirmed: true
+            },
+            {
+                name: 'Matt K',
+                isConfirmed: false
             }
         ]
     }
@@ -54,32 +59,8 @@ class App extends Component {
                             </tr>
                         </tbody>
                     </table>
-                    <ul>
-                        <li className="pending"><span>Safia</span></li>
-                        <li className="responded"><span>Iver</span>
-                            <label>
-                                <input type="checkbox" checked /> Confirmed
-                            </label>
-                            <button>edit</button>
-                            <button>remove</button>
-                        </li>
-                        <li className="responded">
-                            <span>Corrina</span>
-                            <label>
-                                <input type="checkbox" checked /> Confirmed
-                            </label>
-                            <button>edit</button>
-                            <button>remove</button>
-                        </li>
-                        <li>
-                            <span>Joel</span>
-                            <label>
-                            <input type="checkbox"/> Confirmed
-                            </label>
-                            <button>edit</button>
-                            <button>remove</button>
-                        </li>
-                    </ul>
+
+                    <GuestList guests={this.state.guests} />
                 </div>
             </div>
         );
