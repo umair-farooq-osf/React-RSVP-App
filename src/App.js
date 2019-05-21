786ec19eed88ea8da2dbed94f9c87116346a120e
Umair Farooq <umair.farooq@osf-commerce.com>
2019-05-21 13:09:33 +0500

    React by example: Pending guest feature

--- src/App.js
+++ src/App.js
@@ -147,6 +147,7 @@ class App extends Component {
                         removeGuestAt={ this.removeGuestAt }
                         setNameAt={ this.setNameAt }
                         isFiltered={ this.state.isFiltered }
+                        pendingGuest={ this.state.pendingGuest }
                     />
                 </div>
             </div>
