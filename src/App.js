13bcbefae94ac14616c32cf4c4b0ffc59e3e6daf
Umair Farooq <umair.farooq@osf-commerce.com>
2019-05-21 11:50:28 +0500

    React by example: Guest component introduced. handler added to update set on onChange of confirmation checkbox

--- src/App.js
+++ src/App.js
@@ -21,6 +21,20 @@ class App extends Component {
         ]
     }
 
+    toggleConfirmationAt = indexToChange =>
+        this.setState({
+            guests: this.state.guests.map((guest, index) => {
+                if (index === indexToChange) {
+                    return {
+                        ...guest,
+                        isConfirmed: !guest.isConfirmed
+                    };
+                }
+
+                return guest;
+            })
+        });
+
     getTotalInvited = () => this.state.guests.length;
     // getAttendingGuests = () =>
     // getUnconfirmedGuests = () =>
@@ -60,7 +74,10 @@ class App extends Component {
                         </tbody>
                     </table>
 
-                    <GuestList guests={this.state.guests} />
+                    <GuestList
+                        guests={this.state.guests}
+                        toggleConfirmationAt={this.toggleConfirmationAt}
+                    />
                 </div>
             </div>
         );
