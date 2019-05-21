56a006659800b89ef9e235c8c9dc2183f19b2dea
Umair Farooq <umair.farooq@osf-commerce.com>
2019-05-21 14:46:29 +0500

    Updates

---
+++ precache-manifest.6cd4ea134c19bf85224837d45573d872.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2d3ba7a7f31a40e6183288e1a291982",
+    "url": "/React-RSVP-App/index.html"
+  },
+  {
+    "revision": "51b615d1d258a4d348d0",
+    "url": "/React-RSVP-App/static/css/main.2e4dc2ab.chunk.css"
+  },
+  {
+    "revision": "4a036c1dc56cec7e181b",
+    "url": "/React-RSVP-App/static/js/2.67c1c55e.chunk.js"
+  },
+  {
+    "revision": "51b615d1d258a4d348d0",
+    "url": "/React-RSVP-App/static/js/main.e6fd94a8.chunk.js"
+  },
+  {
+    "revision": "8dd011baffd01d07c19d",
+    "url": "/React-RSVP-App/static/js/runtime~main.dd24b7e6.js"
+  },
+  {
+    "revision": "be92ab00f0f555419adb491aa392b9ca",
+    "url": "/React-RSVP-App/static/media/header-bg.be92ab00.jpg"
+  }
+]);
